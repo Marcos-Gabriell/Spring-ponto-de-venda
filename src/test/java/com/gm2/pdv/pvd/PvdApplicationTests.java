940a87c88f7f078dcd5d3b7363df1dd1ffd5c93c
mgabr <marcosgabriel79355@gmail.com>
2023-12-04 18:23:31 -0300

    Initial commit

---
+++ src/test/java/com/gm2/pdv/pvd/PvdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm2.pdv.pvd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PvdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
