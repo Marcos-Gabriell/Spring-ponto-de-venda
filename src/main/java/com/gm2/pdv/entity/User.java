68ef019cddd979fb843a64b0d358b86148698a12
mgabr <marcosgabriel79355@gmail.com>
2023-12-05 18:17:33 -0300

    mapeando entity

--- src/main/java/com/gm2/pdv/entity/User.java
+++ src/main/java/com/gm2/pdv/entity/User.java
@@ -1,11 +1,15 @@
 package com.gm2.pdv.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.*;
 
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
+@Table(name = "user")
 public class User {
 
     private Long id;
