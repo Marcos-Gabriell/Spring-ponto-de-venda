36fc3d8dae2a020247d14cd7e787c72685d51274
mgabr <marcosgabriel79355@gmail.com>
2023-12-05 18:22:53 -0300

    Fianlziando persistencia na class Product

--- src/main/java/com/gm2/pdv/entity/Product.java
+++ src/main/java/com/gm2/pdv/entity/Product.java
@@ -1,7 +1,15 @@
 package com.gm2.pdv.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table
+@Entity
 public class Product {
 }
