7a63abd725b19edb96d47074a40663673bebc8ad
mgabr <marcosgabriel79355@gmail.com>
2023-12-05 21:29:20 -0300

    Implementando as anotações

--- src/main/java/com/gm2/pdv/entity/Sale.java
+++ src/main/java/com/gm2/pdv/entity/Sale.java
@@ -1,4 +1,17 @@
 package com.gm2.pdv.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "sale")
+@Entity
 public class Sale {
 }
