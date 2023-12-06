fcbf8d5297e46c47609902baec6ce0179d326d19
mgabr <marcosgabriel79355@gmail.com>
2023-12-06 15:04:41 -0300

    Iniciando a persistencia

--- src/main/java/com/gm2/pdv/entity/ItemSale.java
+++ src/main/java/com/gm2/pdv/entity/ItemSale.java
@@ -1,7 +1,6 @@
 package com.gm2.pdv.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,4 +11,8 @@ import lombok.NoArgsConstructor;
 @Table(name = "item_sale")
 @Entity
 public class ItemSale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
