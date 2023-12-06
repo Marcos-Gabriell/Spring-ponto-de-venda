2a9e0e7db243dd85ccb4164eb5ae88064e089ec9
mgabr <marcosgabriel79355@gmail.com>
2023-12-06 15:13:18 -0300

    persistencia do produoto

--- src/main/java/com/gm2/pdv/entity/ItemSale.java
+++ src/main/java/com/gm2/pdv/entity/ItemSale.java
@@ -20,5 +20,7 @@ public class ItemSale {
     @JoinColumn(name = "sale_id", nullable = false)
     private Sale sale;
 
-
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
 }
