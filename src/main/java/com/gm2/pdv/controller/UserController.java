ff7976f0337d0cbb204e7816d30d060a275d26fc
mgabr <marcosgabriel79355@gmail.com>
2023-12-07 13:34:40 -0300

    Criando os primeiros endpoints

--- src/main/java/com/gm2/pdv/controller/UserController.java
+++ src/main/java/com/gm2/pdv/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.gm2.pdv.controller;
 
+import com.gm2.pdv.entity.User;
 import com.gm2.pdv.respository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/user")
@@ -19,5 +23,17 @@ public class UserController {
     }
 
     @GetMapping()
-    public ResponseEntity get
+    public ResponseEntity getAll() {
+        return new ResponseEntity(userRepository.findAll(), HttpStatus.OK)
+    }
+
+    @PostMapping()
+
+    public ResponseEntity post(@ResponseBody quest User user) {
+        try{
+            retrun new ResponseEntity<>(userRepository.save(user), HttpStatus.CREATED);
+        }catch (Exception error) {
+            return new ResponseEntity(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
