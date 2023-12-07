600e68bdbe76f6f93f165758ebd13a8c26dbc383
mgabr <marcosgabriel79355@gmail.com>
2023-12-07 13:10:29 -0300

    Erro

--- src/main/java/com/gm2/pdv/respository/ProductRepository.java
+++ src/main/java/com/gm2/pdv/respository/ProductRepository.java
@@ -1,8 +1,11 @@
-package com.gm2.pdv.respository;
+package com.gm2.pdv.repository;
 
+import com.gm2.pdv.model.ItemSale; // Asegúrate de importar la clase ItemSale correctamente
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ProductRepository extends  extends JpaRepository<ItemSale, Long {
+public interface ProductRepository extends JpaRepository<ItemSale, Long> {
+    // Puedes agregar consultas personalizadas aquí si es necesario
 }
+
