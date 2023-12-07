870ca84eaf25ae88b6b06a5e7dff30c8f4079782
mgabr <marcosgabriel79355@gmail.com>
2023-12-07 13:14:07 -0300

    Corração nas etitys

--- src/main/java/com/gm2/pdv/respository/ItemSaleRepository.java
+++ src/main/java/com/gm2/pdv/respository/ItemSaleRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ItemSaleRepository extends JpaRepository<ItemSale, Long> {
+public interface ItemSaleRepository extends JpaRepository<ItemSale, Long> {
 }
--- src/main/java/com/gm2/pdv/respository/ProductRepository.java
+++ src/main/java/com/gm2/pdv/respository/ProductRepository.java
@@ -1,11 +1,8 @@
-package com.gm2.pdv.repository;
+package com.gm2.pdv.respository;
 
-import com.gm2.pdv.model.ItemSale; // Asegúrate de importar la clase ItemSale correctamente
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<ItemSale, Long> {
-    // Puedes agregar consultas personalizadas aquí si es necesario
+public class ProductRepository extends JpaRepository<ItemSale, Long> {
 }
-
