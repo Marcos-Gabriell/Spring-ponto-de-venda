e8faecc589d11a7bd6d81e6958d4dd145e25becb
mgabr <marcosgabriel79355@gmail.com>
2023-12-07 13:59:14 -0300

    Erro ao rodar projeto

--- src/main/java/com/gm2/pdv/controller/UserController.java
+++ src/main/java/com/gm2/pdv/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.gm2.pdv.controller;
 
 import com.gm2.pdv.entity.User;
+
 import com.gm2.pdv.respository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,30 +10,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody; // Adicionado para corrigir o erro
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/user")
-
 public class UserController {
 
     private UserRepository userRepository;
 
+    // Corrigido o nome do parâmetro para userRepository
     public UserController(@Autowired UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    @GetMapping()
+    @GetMapping
     public ResponseEntity getAll() {
-        return new ResponseEntity(userRepository.findAll(), HttpStatus.OK)
+        return new ResponseEntity(userRepository.findAll(), HttpStatus.OK);
     }
 
-    @PostMapping()
-
-    public ResponseEntity post(@ResponseBody quest User user) {
-        try{
-            retrun new ResponseEntity<>(userRepository.save(user), HttpStatus.CREATED);
-        }catch (Exception error) {
+    @PostMapping
+    public ResponseEntity post(@RequestBody User user) { // Corrigido o tipo do parâmetro para User
+        try {
+            // Corrigido "retrun" para "return"
+            return new ResponseEntity<>(userRepository.save(user), HttpStatus.CREATED);
+        } catch (Exception error) {
             return new ResponseEntity(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
