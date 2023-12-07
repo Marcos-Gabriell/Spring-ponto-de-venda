ecfa8f366a6b5eba9f3dcc8a644a28265af047ae
mgabr <marcosgabriel79355@gmail.com>
2023-12-07 13:14:29 -0300

    Correções nas entitys

--- src/main/java/com/gm2/pdv/respository/ProductRepository.java
+++ src/main/java/com/gm2/pdv/respository/ProductRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ProductRepository extends JpaRepository<ItemSale, Long> {
+public interface ProductRepository extends JpaRepository<ItemSale, Long> {
 }
