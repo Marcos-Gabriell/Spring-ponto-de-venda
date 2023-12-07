ddc3c57fd27514cfa4c2e2aae12d8a79ed3cf8bd
mgabr <marcosgabriel79355@gmail.com>
2023-12-07 13:14:57 -0300

    Correção no import

--- src/main/java/com/gm2/pdv/respository/ProductRepository.java
+++ src/main/java/com/gm2/pdv/respository/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.gm2.pdv.respository;
 
+import com.gm2.pdv.entity.ItemSale;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
