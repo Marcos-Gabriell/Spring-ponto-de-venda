8ff9e9d32f349dd6394747cc2faa6bf11923090b
mgabr <marcosgabriel79355@gmail.com>
2023-12-10 12:53:05 -0300

    Começando a criar class de product

---
+++ src/main/java/com/gm2/pdv/controller/ProductController.java
@@ -0,0 +1,10 @@
+package com.gm2.pdv.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+
+}
