b58a453da0890a1f2aab0b57c51870aa247a8ad2
mgabr <marcosgabriel79355@gmail.com>
2023-12-11 14:37:27 -0300

    Avançando na implementação do DTO e dos serviços

--- src/main/java/com/gm2/pdv/service/SaleService.java
+++ src/main/java/com/gm2/pdv/service/SaleService.java
@@ -1,4 +1,35 @@
 package com.gm2.pdv.service;
 
+import com.gm2.pdv.dto.ProductDTO;
+import com.gm2.pdv.dto.SaleDTO;
+import com.gm2.pdv.entity.ItemSale;
+import com.gm2.pdv.entity.Sale;
+import com.gm2.pdv.entity.User;
+import com.gm2.pdv.respository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class SaleService {
+
+    private final UserRepository userRepository;
+
+    public long save(SaleDTO sale) {
+        User user = userRepository.findById(sale.getUserid()).getClass();
+
+        Sale newSale = new Sale();
+        newSale.setUser(user);
+        newSale.setDate(LocalDate.now());
+        newSale.ser
+
+    }
+
+    private List<ItemSale> getItemSale(List<ProductDTO> products) {
+
+    }
 }
