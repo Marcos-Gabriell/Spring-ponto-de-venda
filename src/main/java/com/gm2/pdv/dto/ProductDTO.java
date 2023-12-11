ad3055fdd37618c98c97f4f9c133233ebe913488
mgabr <marcosgabriel79355@gmail.com>
2023-12-11 14:23:49 -0300

    Começando a usar o DTO(Data Transfer Object)

---
+++ src/main/java/com/gm2/pdv/dto/ProductDTO.java
@@ -0,0 +1,15 @@
+package com.gm2.pdv.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+
+    private Long productid;
+
+    private int quantity;
+}
--- src/main/java/com/gm2/pdv/respository/ProductRepository.java
+++ src/main/java/com/gm2/pdv/respository/ProductRepository.java
@@ -1,9 +1,10 @@
 package com.gm2.pdv.respository;
 
-import com.gm2.pdv.entity.ItemSale;
+
+import com.gm2.pdv.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<ItemSale, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
