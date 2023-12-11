e1c28000e9b7c540ee87303334bfa4a046020f6b
mgabr <marcosgabriel79355@gmail.com>
2023-12-11 09:49:02 -0300

    voltando a criar a criar class de product

--- src/main/java/com/gm2/pdv/controller/ProductController.java
+++ src/main/java/com/gm2/pdv/controller/ProductController.java
@@ -1,10 +1,26 @@
 package com.gm2.pdv.controller;
 
+import com.gm2.pdv.respository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/product")
 public class ProductController {
 
+
+    private ProductRepository productRepository;
+
+    public ProductRepository(@Autowired ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping()
+    public ResponseEntity getAll() {
+        return  new ResponseEntity(productRepository.findAll(), HttpStatus.OK);
+    }
 }
