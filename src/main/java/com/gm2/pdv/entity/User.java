54c182f74e91c368bbdb6fd2a4e9bdbda2b247c2
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-12-13 13:13:23 -0300

    Finalizando entity de user

--- src/main/java/com/gm2/pdv/entity/User.java
+++ src/main/java/com/gm2/pdv/entity/User.java
@@ -1,15 +1,24 @@
 package com.gm2.pdv.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
+@Entity
+@Table(name = "user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String neme;
+    @Column(length = 100, nullable = false)
+    private String name;
 
     private boolean isEnabled;
 }
