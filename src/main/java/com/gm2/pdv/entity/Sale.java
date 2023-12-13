73ca6c966ee8c2168e468863bfa5265808de9eca
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-12-13 14:28:25 -0300

    Criação da entity de sale e da itemsale

---
+++ src/main/java/com/gm2/pdv/entity/Sale.java
@@ -0,0 +1,27 @@
+package com.gm2.pdv.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "sale")
+@Entity
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "sale_date", nullable = false)
+    private LocalDate date;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
