6ded7e163d501cf9c4f39865f6e775c5b7b59d75
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-12-13 21:44:31 -0300

    Correção no user repository

--- src/main/java/com/gm2/pdv/repository/UserRepository.java
+++ src/main/java/com/gm2/pdv/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.gm2.pdv.repository;
 
 import com.gm2.pdv.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
