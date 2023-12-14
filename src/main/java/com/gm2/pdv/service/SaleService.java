c9a175427b8eee39c3ab0b7c3da8e0728684aa63
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-12-14 13:32:09 -0300

    Erro no sale DTO

--- src/main/java/com/gm2/pdv/service/SaleService.java
+++ src/main/java/com/gm2/pdv/service/SaleService.java
@@ -3,38 +3,63 @@ package com.gm2.pdv.service;
 import com.gm2.pdv.dto.ProductDTO;
 import com.gm2.pdv.dto.SaleDTO;
 import com.gm2.pdv.entity.ItemSale;
+import com.gm2.pdv.entity.Product;
 import com.gm2.pdv.entity.Sale;
 import com.gm2.pdv.entity.User;
+import com.gm2.pdv.repository.ItemSaleRepository;
+import com.gm2.pdv.repository.ProductRepository;
+import com.gm2.pdv.repository.SaleRepository;
 import com.gm2.pdv.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class SaleService {
 
-
     private final UserRepository userRepository;
+    private final ProductRepository productRepository;
+    private final SaleRepository saleRepository;
+    private final ItemSaleRepository itemSaleRepository;
 
-
+    @Transactional
     public long save(SaleDTO sale) {
-
         User user = userRepository.findAllById(sale.getUserid()).get();
-
-
 
         Sale newSale = new Sale();
         newSale.setUser(user);
         newSale.setDate(LocalDate.now());
-        newSale.setItems();
+        List<ItemSale> items = getItemSale(sale.getItems());
+
+        newSale = saleRepository.save(newSale);
+
+        saveItemSale(items, newSale);
+
+        return newSale.getId();
     }
 
-    private List<ItemSale> getItemSale(List<ProductDTO> products){
-
+    public void saveItemSale(List<ItemSale> items, Sale sale) {
+        for (ItemSale item : items) {
+            item.setSale(newSale);
+            itemSaleRepository.save(item);
+        }
     }
 
+    private List<ItemSale> getItemSale(List<ProductDTO> products) {
+        return products.stream().map(item -> {
+            Product product = productRepository.getReferenceById(item.getProductid());
+
+            ItemSale itemSale = new ItemSale();
+            itemSale.setProduct(product);
+            itemSale.setQuantity(item.getQuantity());
+
+            return itemSale;
+        }).collect(Collectors.toList());
+    }
 }
