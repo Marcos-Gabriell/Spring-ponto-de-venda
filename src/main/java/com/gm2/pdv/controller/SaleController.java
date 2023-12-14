5ef8ce6aaf13008970170d0240c14fd701343f6e
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-12-14 15:14:55 -0300

    ERRO

--- src/main/java/com/gm2/pdv/controller/SaleController.java
+++ src/main/java/com/gm2/pdv/controller/SaleController.java
@@ -4,6 +4,7 @@ import com.gm2.pdv.dto.SaleDTO;
 import com.gm2.pdv.service.SaleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,5 +21,11 @@ public class SaleController {
     @PostMapping()
     public ResponseEntity post(@RequestBody SaleDTO saleDTO) {
 
+        try {
+            long id = saleService.save(saleDTO);
+            return new ResponseEntity("venda realizada com sucesso: " + id, HttpStatus.CREATED);
+        } catch (Exception error){
+            return new ResponseEntity(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
