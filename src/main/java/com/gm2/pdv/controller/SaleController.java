310b6fa93d5fae783627d17455e4b1bcecbc0d4f
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-12-14 13:35:53 -0300

    Iniciando criação da sale controller

--- src/main/java/com/gm2/pdv/controller/SaleController.java
+++ src/main/java/com/gm2/pdv/controller/SaleController.java
@@ -1,9 +1,12 @@
 package com.gm2.pdv.controller;
 
+import com.gm2.pdv.service.SaleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/sale")
 public class SaleController {
+
+    private SaleService saleService;
 }
