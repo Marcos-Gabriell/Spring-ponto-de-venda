12f0be21c883511950883c499890a31a30c0041f
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-12-13 21:56:37 -0300

    Finalizando endpoits de product

--- src/main/java/com/gm2/pdv/controller/ProductController.java
+++ src/main/java/com/gm2/pdv/controller/ProductController.java
@@ -1,47 +1,48 @@
 package com.gm2.pdv.controller;
 
+import com.gm2.pdv.entity.Product;
+import com.gm2.pdv.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/product")
 public class ProductController {
-    private ProductRepository productRepository;
 
-    public ProductController (@Autowired ProductRepository productRepository) {
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductController(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
-    @GetMapping()
-    public ResponseEntity getAll() {
-        return  new ResponseEntity(productRepository.findAll(), HttpStatus.OK);
+    @GetMapping
+    public ResponseEntity<?> getAll() {
+        return new ResponseEntity<>(productRepository.findAll(), HttpStatus.OK);
     }
 
-
-    @PostMapping()
-    public ResponseEntity post(@RequestBody Product product) {
+    @PostMapping
+    public ResponseEntity<?> post(@RequestBody Product product) {
         try {
-            return new ResponseEntity(productRepository.save(product), HttpStatus.CREATED);
-        } catch (Exception error){
-            return new ResponseEntity(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(productRepository.save(product), HttpStatus.CREATED);
+        } catch (Exception error) {
+            return new ResponseEntity<>(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @PutMapping()
-    public ResponseEntity put(@RequestBody Product product) {
+    @PutMapping
+    public ResponseEntity<?> put(@RequestBody Product product) {
         try {
-            return new ResponseEntity(productRepository.save(product), HttpStatus.OK);
-        } catch (Exception error){
-            return new ResponseEntity(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(productRepository.save(product), HttpStatus.OK);
+        } catch (Exception error) {
+            return new ResponseEntity<>(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @DeleteMapping("{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<String> delete(@PathVariable long id) {
         try {
             productRepository.deleteById(id);
@@ -51,4 +52,3 @@ public class ProductController {
         }
     }
 }
-
