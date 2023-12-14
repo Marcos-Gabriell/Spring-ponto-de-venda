4c4cd506b20cc7b9bb32011185401dcec29d6fdc
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-12-14 08:43:49 -0300

    Criação da controller de venda

---
+++ src/main/java/com/gm2/pdv/controller/SaleController.java
@@ -0,0 +1,9 @@
+package com.gm2.pdv.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sale")
+public class SaleController {
+}
