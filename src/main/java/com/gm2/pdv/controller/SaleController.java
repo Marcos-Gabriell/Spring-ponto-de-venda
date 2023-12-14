72abac23c91dd414ce1a2082b5ae8d7df3b75108
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-12-14 13:38:38 -0300

    Iniciando criação do post

--- src/main/java/com/gm2/pdv/controller/SaleController.java
+++ src/main/java/com/gm2/pdv/controller/SaleController.java
@@ -1,12 +1,24 @@
 package com.gm2.pdv.controller;
 
+import com.gm2.pdv.dto.SaleDTO;
 import com.gm2.pdv.service.SaleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/sale")
+@RequiredArgsConstructor
 public class SaleController {
 
     private SaleService saleService;
+
+    @PostMapping()
+    public ResponseEntity post(@RequestBody SaleDTO saleDTO) {
+
+    }
 }
