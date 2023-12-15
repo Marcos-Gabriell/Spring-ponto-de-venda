ca5f34087267516766b270b0cb6ea15f5ea18cae
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-12-15 09:48:55 -0300

    finalizando a Criação do ProdcutInfoDTO

--- src/main/java/com/gm2/pdv/dto/ProdcutInfoDTO.java
+++ src/main/java/com/gm2/pdv/dto/ProdcutInfoDTO.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProdcutInfoDTO {
+
+    private String description;
+
+    private int quantity;
 }
