d0501e39234fb273e0f16344306738d59b6cab65
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-12-15 12:00:23 -0300

    Finalizando get de venda

--- src/main/java/com/gm2/pdv/controller/SaleController.java
+++ src/main/java/com/gm2/pdv/controller/SaleController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SaleController {
 
     private final SaleService saleService;
+
+
+    @GetMapping()
+    public ResponseEntity getAll() {
+        return new ResponseEntity(saleService.findAll(), HttpStatus.OK);
+    }
+
 
     @PostMapping()
     public ResponseEntity post(@RequestBody SaleDTO saleDTO) {
