46ae162a7a53af6ff5870dfb5e05dad79fca4b6f
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-12-18 13:40:09 -0300

    Criando os construtores

--- src/main/java/com/gm2/pdv/dto/ResponseDTO.java
+++ src/main/java/com/gm2/pdv/dto/ResponseDTO.java
@@ -2,6 +2,8 @@ package com.gm2.pdv.dto;
 
 import lombok.Getter;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
 
 public class ResponseDTO<T> {
@@ -11,4 +13,14 @@ public class ResponseDTO<T> {
 
     @Getter
     private T data;
+
+    public ResponseDTO(List<String> message, T data) {
+        this.message = message;
+        this.message = data;
+    }
+
+    public ResponseDTO(String message, T data) {
+        this.message = Arrays.asList(message);
+        this.message = data;
+    }
 }
