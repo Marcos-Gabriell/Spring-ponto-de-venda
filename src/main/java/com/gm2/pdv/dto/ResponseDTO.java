02365fb4496f7da19510fbe4e556c83961c7f023
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-12-18 13:50:19 -0300

    Correção no Erro no responseDTO

--- src/main/java/com/gm2/pdv/dto/ResponseDTO.java
+++ src/main/java/com/gm2/pdv/dto/ResponseDTO.java
@@ -2,25 +2,24 @@ package com.gm2.pdv.dto;
 
 import lombok.Getter;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
 public class ResponseDTO<T> {
 
     @Getter
-    private List<String> message;
+    private List<String> messages;
 
     @Getter
     private T data;
 
-    public ResponseDTO(List<String> message, T data) {
-        this.message = message;
-        this.message = data;
+    public ResponseDTO(List<String> messages, T data) {
+        this.messages = messages;
+        this.data = data;
     }
 
     public ResponseDTO(String message, T data) {
-        this.message = Arrays.asList(message);
-        this.message = data;
+        this.messages = Arrays.asList(message);
+        this.data = data;
     }
 }
