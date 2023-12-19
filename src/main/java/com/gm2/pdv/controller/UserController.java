8cc625cd0eca31de9d092bc380fd26a69823222b
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-12-19 16:20:51 -0300

    Problema no user controller

--- src/main/java/com/gm2/pdv/controller/UserController.java
+++ src/main/java/com/gm2/pdv/controller/UserController.java
@@ -2,7 +2,7 @@ package com.gm2.pdv.controller;
 
 import com.gm2.pdv.entity.User;
 import com.gm2.pdv.repository.UserRepository;
-import com.gm2.pdv.service.UserSevice;
+import com.gm2.pdv.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,21 +16,21 @@ import java.util.Optional;
 @RequestMapping("/user")
 public class UserController {
 
-    private final UserSevice userSevice;
+    private final UserService userService;
 
     @Autowired
-    public UserController(UserSevice userSevice) {
-        this.userSevice = userSevice;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping
-    public ResponseEntity<?> getAll() { return new ResponseEntity<>(userSevice.findAll(), HttpStatus.OK);}
+    public ResponseEntity<?> getAll() { return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);}
 
     @PostMapping
     public ResponseEntity<?> post(@RequestBody User user) {
         try {
             user.setEnabled(true);
-            return new ResponseEntity<>(userSevice.save(user), HttpStatus.CREATED);
+            return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
         } catch (Exception error) {
             return new ResponseEntity<>(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
