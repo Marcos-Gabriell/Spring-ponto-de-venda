23675ba44f8eab23b998687bb5d6e8a790d4fb09
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-01-08 16:29:59 -0300

    inicando alterações nas reposta dos controllers

--- src/main/java/com/gm2/pdv/dto/ResponseDTO.java
+++ src/main/java/com/gm2/pdv/dto/ResponseDTO.java
@@ -5,16 +5,14 @@ import lombok.Getter;
 import java.util.Arrays;
 import java.util.List;
 
-public class ResponseDTO<T> {
+public class ResponseDTO {
 
     @Getter
     private List<String> messages;
 
-
     public ResponseDTO(List<String> messages) {
         this.messages = messages;
     }
-
     public ResponseDTO(String message) {
         this.messages = Arrays.asList(message);
     }
